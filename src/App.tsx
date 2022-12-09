1f8b88737e9b5f59b6a8d26b40f274fe1a4f7eb6
Conley Brown <conleybrown@conleys-mbp.lan>
2022-12-09 13:41:14 -0600

    create basic landing page with background image, header, text and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import backgrounddark from './images/backgrounddark.jpg'
 
 function App() {
   return (
-    <div className="App">
-     Welcome
+    <div style = {{ backgroundImage: `url(${backgrounddark})`,width: '100vw', height: '100vh',}}>
+      <div className= "w-screen">
+      <h1 className = "text-3xl font-bold underline text-center text-white">
+        Welcome
+      </h1>
+      </div>
+      <div className = "w-screen">
+      <p className ="text-white text-center">
+        Connect your wallet to get started!
+      </p>
+      </div>
+      <div className = "w-screen text-center">
+        <button className = "text-white ">Connect Wallet</button>
+      </div>
     </div>
   );
 }
